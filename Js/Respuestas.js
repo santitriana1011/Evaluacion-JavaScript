f4f875400afda4b7e63daa4968c7a1c1db34f80f
santitriana1011 <41800013+santitriana1011@users.noreply.github.com>
2019-03-11 16:42:26 -0500

    Initial commit

---
+++ Js/Respuestas.js
@@ -0,0 +1,57 @@
+//Primer punto: Definicion de Funciones
+// bienvenida();
+// function bienvenida() {
+//     console.log("Bienvenidos");
+// }
+
+// //Segunda opcion
+
+// const suma = function() {
+//     console.log(10+20);
+// }
+
+// suma();
+
+
+// //Tercera Opcion Cuando Tiene Parametros 
+
+// function ejecuta(numero,numero2) {
+//     var mass = numero+numero2
+//     console.log("mass es = a ",mass);
+// }
+
+// ejecuta(12,25);
+
+//Cuarta Opcion  sin ponerle algun parametro
+ 
+// function ejemplo(){
+//         var num = 10;
+//          return num;
+//      }
+    
+//     ejemplo();
+//     var define = ejemplo();
+//     console.log("definicion= "+ define);
+
+//Quinta Opcion Una Funcion Anonima (Sin nombre)
+
+// var soluciona = function (priNumero, SeNumero) {
+//     var soluciona = priNumero+SeNumero;
+//     return soluciona;
+// }
+
+// console.log("resultado suma es:  ", soluciona (30, 20));
+
+//Sexta Opcion Funcion LAMDA
+
+// var sumatoria = (numer1, numer2) => {
+//         var respuesta = numer1+numer2;
+//         return respuesta;
+//     }
+//     console.log("Sumatoria con Lamda", sumatoria(90,30));
+
+
+//Clasesssss
+
+
+
